b36c36819854adb2414795184bb72715e1b7c9e7
Vitalik1800 <51854958+Vitalik1800@users.noreply.github.com>
2025-10-07 18:09:50 +0300

    ci.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,9 +35,19 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
