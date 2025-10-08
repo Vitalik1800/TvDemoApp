6047e3829ac7c913f7bc04dafbc9ab03e7287e89
Vitalik1800 <51854958+Vitalik1800@users.noreply.github.com>
2025-10-08 18:41:20 +0300

    Delete app/src/main/java/com/vs18/tvdemoapp/AppDatabase.kt

--- app/src/main/java/com/vs18/tvdemoapp/AppDatabase.kt
+++
@@ -1,8 +0,0 @@
-package com.vs18.tvdemoapp
-
-import androidx.room.*
-
-@Database(entities = [Movie::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun movieDao(): MovieDao
-}
